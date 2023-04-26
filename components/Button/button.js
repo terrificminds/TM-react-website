e037e3279fde95a1a891de2d10f35303517e2485
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-04-26 14:19:02 +0530

    implemented Link component for Button

--- components/Button/button.js
+++ components/Button/button.js
@@ -1,10 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Link from "next/link";
 
-export default function Button({text, className, styleText, style}) {
+
+export default function Button({text, className, styleText, href, style}) {
     return(
-        <button className={className} style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+        <Link href={`${href}`}>
+            <button className={className} style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
             <p style={styleText}>{text}</p><span style={{marginLeft:'.4rem', display:'flex', alignItems:'center', justifyContent:'center'}}><img src="/ctaArrowWhite.svg" alt="arrow" /></span>
-        </button>
+            </button>
+        </Link>
     )
 }
--- components/TitleC/TitleC.js
+++ components/TitleC/TitleC.js
@@ -1,11 +1,11 @@
 import Button from "../Button/button";
 
 
-export default function TitleC({title, text, className, style}) {
+export default function TitleC({title, text, className, style, href}) {
     return(
         <div className='eyeBrow'>
            <div className='eyeBrowText' style={style}><p>{title}</p></div>
-           <div className='btnDesk'><Button text={text} className={className} /></div>
+           <div className='btnDesk'><Button href={href} text={text} className={className} /></div>
         </div>
     )
 }
